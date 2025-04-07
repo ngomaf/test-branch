d8da60d3fe9be2bab5afdb307a044b8170c1e884
Ngoma M. Fortuna <ngomafortuna@gmail.com>
2025-04-07 16:05:58 +0100

    commit feature B

---
+++ feat_a/contact.php
@@ -0,0 +1,15 @@
+<?php
+
+$contacts = array();
+
+$contacts[] = [
+	'email' => 'info@branch.ao',
+	'phone' => '+244 222222222',
+	'address' => 'caala/Huambo/Angola'
+];
+
+echo PHP_EOL;
+
+var_dump($contacts);
+
+echo PHP_EOL;
